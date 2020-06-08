6d83c77ecfb7a4c57ddc83f4728105d8a44608e9
Rolkyssoh <juventussoh@gmail.com>
2020-06-08 11:51:20 +0200

    updationg our app with a shop page, collection preview component and collection item component

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -49,6 +49,9 @@ class Directory extends React.Component {
     render() {
         return (
             <div className="directory-menu">
+                {/* {this.state.sections.map(({ title, imageUrl, id, size, linkUrl }) => (
+                    <MenuItem key={id} title={title} imge={imageUrl} size={size} linkUrl={linkUrl} />
+                ))} */}
                 {this.state.sections.map(({ id, ...otherSectionProps }) => (
                     <MenuItem key={id} {...otherSectionProps} />
                 ))}
