56344587ca01d4159c7da45de41e1391be3cba46
Rolkyssoh <juventussoh@gmail.com>
2020-07-05 16:27:27 +0200

    moved our directory and shop data into its own reducer, created respected flux flow for directory/shop

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,30 +1,18 @@
 import React from 'react';
-import SHOP_DATA from './shop.data';
+import { Route } from 'react-router-dom';
+import CollectionOverview from '../../components/collection-overview/collection-overview.component';
+// import { connect } from 'react-redux';
+// import { createStructuredSelector } from 'reselect';
 
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+// import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+import CollectionPage from '../collection/collection.component';
 
-class ShopPage extends React.Component {
-    constructor(props) {
-        super(props);
+const ShopPage = ({ match }) => (
+    <div className='shop-page'>
+        <Route exat path={`${match.path}`} compoent={CollectionOverview} />
+        <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+    </div>
+);
 
-        this.state = {
-            collections: SHOP_DATA
-        };
-    }
-
-    render() {
-        const { collections } = this.state;
-
-        return (
-            <div className='shop-page'>
-                {
-                    collections.map(({ id, ...otherCollectionProps }) => (
-                        <CollectionPreview key={id} {...otherCollectionProps} />
-                    ))
-                }
-            </div>
-        );
-    }
-}
 
 export default ShopPage;
